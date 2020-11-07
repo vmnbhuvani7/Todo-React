2e7032904086d84b24dd65d7b143a9a057ac0c98
vmnbhuvani7 <31824451+vmnbhuvani7@users.noreply.github.com>
2020-11-07 16:53:19 +0530

    updated changes

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Task from './Task';
 
-ReactDOM.render(< App />, document.getElementById('root'));
+ReactDOM.render(< Task />, document.getElementById('root'));
